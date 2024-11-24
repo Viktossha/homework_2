b88ef243c5b3367faa8e792d6ff47341bc5a8719
Vika <vikapetrova8@gmail.com>
2024-11-24 14:57:44 +0300

    HW8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,15 +4,36 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => {
     switch (action.type) {
         case 'sort': { // by name
-
-            return state // need to fix
+            let copyState = [...state]
+            if (action.payload === 'up') {
+                copyState.sort((a, b) => {
+                    if (a.name > b.name) {
+                        return 1
+                    }
+                    if (a.name < b.name) {
+                        return -1
+                    }
+                    return 0
+                })
+            }
+            if (action.payload === 'down') {
+                copyState.sort((a, b) => {
+                    if (a.name > b.name) {
+                        return -1
+                    }
+                    if (a.name < b.name) {
+                        return 1
+                    }
+                    return 0
+                })
+            }
+            return copyState
         }
         case 'check': {
-
-            return state // need to fix
+            return state.filter(u => u.age > 18)
         }
         default:
             return state
